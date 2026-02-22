822fa4f53d9441b57e8e26e70152bc626cc9792f
Kiran Kumar H.M <kirankumarhm2004@gmail.com>
2026-02-22 20:17:08 +0530

    Create MessagingStreamApplication class for Spring Boot

---
+++ event-processor/src/main/java/com/example/messaging/stream/MessagingStreamApplication.java
@@ -0,0 +1,12 @@
+package com.example.messaging.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingStreamApplication.class, args);
+    }
+}
